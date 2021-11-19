c9af10817801343908e1acfaf0ad05e529ec3b9d
Richar Urbano <richar.urbano@hotmail.com>
2021-11-18 23:03:44 -0500

    Add basic GraphQL Schema

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import express from "express";
 
+// GraphQL
+import { graphqlHTTP } from "express-graphql";
+import schema from "./schema";
+
 const app = express();
 const port = parseInt(process.env.PORT, 10) || 4040;
 
@@ -8,7 +12,21 @@ app.get("/", (req, res) => {
     res.send("GET Request Called");
 });
 
+// Resolvers
+const root = {
+    hola: () => {
+        return 'Hello world!';
+    }
+};
+
+// The root provides a resolver function for each API endpoint
+app.use("/graphql", graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+
 // Listen and running server
 app.listen(app.get("port"), () => {
-    console.log(`Listening to requests on http://localhost:${app.get("port")}`);
+    console.log(`Running a GraphQL API server at http://localhost:${app.get("port")}/graphql`);
 });
