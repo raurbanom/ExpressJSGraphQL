4b1ee0072a66637670f65db9453c6c73e7b00157
Richar Urbano <richar.urbano@hotmail.com>
2021-11-24 22:46:08 -0500

    Add CRUD using Mongoose

--- data/resolvers.js
+++ data/resolvers.js
@@ -4,8 +4,17 @@ import { Client } from "./db";
 // Resolvers
 export const resolvers = {
     Query: {
+        getClients: (_, { limit }) => {
+            return Client.find({}).limit(limit);
+        },
+
         getClient: (_, { id }) => {
-            return new Client(id, clientsDB[id]);
+            return new Promise((resolve, rejects) => {
+                Client.findById({ _id: id }, (error, client) => {
+                    if (error) rejects(error);
+                    resolve(client);
+                });
+            });
         }
     },
     Mutation: {
@@ -26,6 +35,24 @@ export const resolvers = {
                 newClient.save((error) => {
                     if (error) rejects(error);
                     resolve(newClient);
+                })
+            });
+        },
+
+        updateClient: (_, { input }) => {
+            return new Promise((resolve, rejects) => {
+                Client.findOneAndUpdate({ _id: input.id }, input, { new: true }, (error, client) => {
+                    if (error) rejects(error);
+                    resolve(client)
+                });
+            });
+        },
+
+        deleteClient: (_, { id }) => {
+            return new Promise((resolve, rejects) => {
+                Client.findOneAndRemove({ _id: id }, (error) => {
+                    if (error) rejects(error);
+                    resolve("Client has been deleted!")
                 })
             });
         }
